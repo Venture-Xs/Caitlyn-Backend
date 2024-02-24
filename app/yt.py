c34ab675beeebe5577a659f7f9d150860d1d45fd
aswin-madhav <aswinmadhav007@gmail.com>
2024-02-24 18:24:00 +0530

    bot changed

--- app/yt.py
+++ app/yt.py
@@ -6,7 +6,7 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 
 yid = str(input("Enter YouTube video ID: "))
-def bot(yid, is_query = False , query="") :
+def bot(yid, is_query = False , query = "", generateTest = False) :
     # Get transcript of YouTube video
     try:
         tx = YouTubeTranscriptApi.get_transcript(video_id=str(yid), languages=['en', 'ru', 'ml'])
@@ -30,6 +30,11 @@ def bot(yid, is_query = False , query="") :
             "role": "user",
             "content": f"Must return an array of JSON objects of the form: {{'reply': 'reply'}} The article is as follows: {transcript} the query about the article is :{str(query)}"
         }
+    elif generateTest:
+        message = {
+            "role": "user",
+            "content": f"Must return an array of JSON objects of the form: {{'questions': [{{'q1':'...','o1':'...',o2:'...','o3':'...',o4:'...','answer':'...'}},{{'q2':'...','o1':'...',o2:'...','o3':'...',o4:'...','answer':'...'}},...]}} The article is as follows: {transcript} generate a mcq test with 5 questions from this article"
+        } 
     else :
         message = {
             "role": "user",
@@ -39,10 +44,11 @@ def bot(yid, is_query = False , query="") :
     try:
         chat_completion = client.chat.completions.create(
             messages= messages,
-            model="gpt-3.5-turbo",
+            model="gpt-4-0125-preview",
         )
 
         reply = chat_completion.choices[0].message.content
+        print(reply)
         reply.replace("\'","\"")
         reply.replace("json","")
         reply = json.loads(reply)
@@ -51,5 +57,5 @@ def bot(yid, is_query = False , query="") :
         print("Error communicating with OpenAI:", e)
         return 0
 
-reply = bot(yid,True,"What is goggins talking about?")
+reply = bot(yid,generateTest=True)
 print(reply)
