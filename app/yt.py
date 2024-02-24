234cab48a088303acb81fd5a85ed7faa9f90649f
aswin-madhav <aswinmadhav007@gmail.com>
2024-02-24 15:15:09 +0530

    bot changed

--- app/yt.py
+++ app/yt.py
@@ -1,17 +1,55 @@
-import pathlib
-import textwrap
-import openai
+import os
+import json
+from openai import OpenAI
+from dotenv import load_dotenv
 from youtube_transcript_api import YouTubeTranscriptApi
 
 
-outls = []
-yid = str(input("Enter youtube video id:"))
-tx = YouTubeTranscriptApi.get_transcript(video_id = yid, languages = ['en','ru','ml'])
+yid = str(input("Enter YouTube video ID: "))
+def bot(yid, is_query = False , query="") :
+    # Get transcript of YouTube video
+    try:
+        tx = YouTubeTranscriptApi.get_transcript(video_id=str(yid), languages=['en', 'ru', 'ml'])
+        transcript = ""
+        for i in tx:
+            transcript += i['text']
+    except Exception as e:
+        print("Error retrieving transcript:", e)
+        exit()
 
-transcript = ""
-for i in tx:
-    transcript += i['text']
+    # Initialize OpenAI client
+    load_dotenv()
+    client = OpenAI(
+        api_key= os.getenv("API_KEY"),
+    )
 
-#print(transcript)
-    
 
+    messages = []
+    if is_query :
+        message = {
+            "role": "user",
+            "content": f"Must return an array of JSON objects of the form: {{'reply': 'reply'}} The article is as follows: {transcript} the query about the article is :{str(query)}"
+        }
+    else :
+        message = {
+            "role": "user",
+            "content": f"Must return an array of JSON objects of the form: {{'summary_of_section': 'summary', 'time_stamp': '...'}} The article is as follows: {transcript}"
+        }
+    messages.append(message)
+    try:
+        chat_completion = client.chat.completions.create(
+            messages= messages,
+            model="gpt-3.5-turbo",
+        )
+
+        reply = chat_completion.choices[0].message.content
+        reply.replace("\'","\"")
+        reply.replace("json","")
+        reply = json.loads(reply)
+        return reply
+    except Exception as e:
+        print("Error communicating with OpenAI:", e)
+        return 0
+
+reply = bot(yid,True,"What is goggins talking about?")
+print(reply)
