4bc7ab27edbfb6260c9c4b2e8dd26953d70cd031
aswin-madhav <aswinmadhav007@gmail.com>
2024-02-25 00:10:35 +0530

    audio bot.py

--- app/routers/audio_bot.py
+++ app/routers/audio_bot.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, StreamingResponse
 from ..abfunc.stt import stt
 from ..abfunc.ttsMal import tts_mal
 from ..abfunc.ttsoai import tts_en
@@ -14,4 +14,17 @@ router = APIRouter()
 async def audio_bot(audio_bot_req : Audio_bot_req):
     if(audio_bot_req.lang=="en"):
         query = transcript(audio_bot_req.audio_file)
-        bot(url=audio_bot_req.url, )
+        reply = bot(url = audio_bot_req.url, is_query = True, query = query)
+        tts_en(reply, "ttsen.mp3")
+        with open("ttsen.mp3", "rb") as file:
+            audio_data = file.read()
+        return StreamingResponse(audio_data, media_type="audio/mpeg")
+    elif(audio_bot_req.lang=="ml"):
+        query = transcript(audio_bot_req.audio_file)
+        reply = bot(url = audio_bot_req.url, is_query = True, query = query)
+        tts_mal(reply,"ttsmal.mp3")
+        with open("ttsmal.mp3", "rb") as file:
+            audio_data = file.read()
+        return StreamingResponse(audio_data, media_type="audio/mpeg")
+    else:
+        return "Language not available"
